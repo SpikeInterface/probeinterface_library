9279b69f199f2ce2eebe3e3a5fc7dde728f20346
Chris Halcrow <57948917+chrishalcrow@users.noreply.github.com>
2025-10-29 13:08:00 +0000

    Add action to automatically check for new NP probes (#27)

---
+++ scripts/check_for_new_NP_probes.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import shutil
+
+old_dir = Path('../imec')
+new_dir = Path('./neuropixels_library_generated')
+
+existing_probes = list(probe_path.name for probe_path in old_dir.iterdir())
+
+for temp_probe_path in new_dir.iterdir():
+    if temp_probe_path.name not in existing_probes:
+        shutil.copytree(f"{temp_probe_path}", f"../imec/{temp_probe_path.name}")
